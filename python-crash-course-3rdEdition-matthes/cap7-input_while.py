241a93f23450d00c64e3c19d0d21b820071ddb7f
racs1 <racs1@ecomp.poli.br>
2023-09-08 00:25:57 -0300

    cap7 - final

--- python-crash-course-3rdEdition-matthes/cap7-input_while.py
+++ python-crash-course-3rdEdition-matthes/cap7-input_while.py
@@ -62,3 +62,59 @@ print("Sua pizza contém os seguintes ingredientes:")
 for ingrediente in ingredientes:
     print(f"{ingrediente.title()}")
 # %%
+# 7.8 - Lanchonete
+sandwich_orders = ['s1', 's2', 'pastrami', 's3', 's4', 'pastrami', 's5', 'pastrami']
+
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print(f"Seu lanche {current_sandwich.title()} está pronto")
+    finished_sandwiches.append(current_sandwich)
+
+print("Foram servidos os seguintes lanches:")
+
+for sandwich in finished_sandwiches:
+    print(f"{sandwich.title()}")
+
+# %%
+# 7.9 - Sem pastrami
+sandwich_orders = ['s1', 's2', 'Pastrami', 's3', 's4', 'pAstrami', 's5', 'PASTRAMI']
+
+sandwich_orders_lower = []
+
+finished_sandwiches = []
+
+for item in sandwich_orders:
+    sandwich_orders_lower.append(item.lower())
+
+print("Lanchonete está sem 'pastrami'")
+
+while 'pastrami' in sandwich_orders_lower:
+    sandwich_orders_lower.remove('pastrami')
+
+while sandwich_orders_lower:
+    current_sandwich = sandwich_orders_lower.pop()
+    print(f"Seu lanche {current_sandwich.title()} está pronto")
+    finished_sandwiches.append(current_sandwich)
+
+print("Foram servidos os seguintes lanches:")
+
+for sandwich in finished_sandwiches:
+    print(f"{sandwich.title()}")
+
+# %%
+# 7.10 - Férias tão sonhadas
+active = True
+dict = {}
+
+while active:
+    name = input("Qual seu nome?")
+    place = input("Se pudesse visitar qualquer lugar do mundo, para onde iria?")
+    dict[name] = place
+    answer = input("Deseja continuar com a pesquisa? Yes/No")
+    if answer == 'No':
+        active = False
+
+for k, v in dict.items():
+    print(f"O sonho de {k.title()} é visitar {v.title()}")
